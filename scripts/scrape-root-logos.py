cf686cde556eb12f40067752f4805ad58b9f7283
Nick Miethe <miethe.dev@gmail.com>
2023-08-10 11:27:22 -0400

    optimize

--- scripts/scrape-root-logos.py
+++ scripts/scrape-root-logos.py
@@ -2,10 +2,36 @@ import os
 import re
 import shutil
 
-def move_files(source_directory, destination_directory, pattern):
-    # Compile the regex pattern
-    regex = re.compile(pattern)
+SVG_STRING = ".svg"
+SVG_DESTINATION_DIRECTORY = "/Users/miethe/dev/openshift-practice/logos/SVG-Logos/App Frameworks, Language, IDE/Languages"
+PNG_DESTINATION_DIRECTORY = "/Users/miethe/dev/openshift-practice/logos/PNG-Logos/App Frameworks, Language, IDE/Languages"
 
+def get_full_path_regex(file_name, regex, dest_dir, secondary_flag=None, secondary_dir=None):
+    if not regex.search(file_name):
+        # Construct the full path of the file
+        # Construct the destination path
+        if secondary_flag and secondary_flag in file_name:
+            destination_directory = secondary_dir
+        else:
+            destination_directory = dest_dir
+
+        return os.path.join(destination_directory, file_name)
+
+def get_full_path(file_name, pattern, dest_dir, secondary_flag=None, secondary_dir=None):
+    if pattern in file_name:
+        # Construct the full path of the file
+        # Construct the destination path
+        if secondary_flag and secondary_flag in file_name:
+            destination_directory = secondary_dir
+        else:
+            destination_directory = dest_dir
+    else:
+        return None
+    return os.path.join(destination_directory, file_name)
+
+def move_files(source_directory, pattern, destination_directory):
+    # Compile the regex pattern
+    #regex = re.compile(pattern)
     # Check if the destination directory exists, if not create it
     if not os.path.exists(destination_directory):
         os.makedirs(destination_directory)
@@ -14,30 +40,26 @@ def move_files(source_directory, destination_directory, pattern):
     for root, dirs, files in os.walk(source_directory):
         for file_name in files:
             # Check if the file does not match the pattern
-            if not regex.match(file_name):
-                # Construct the full path of the file
-                file_path = os.path.join(root, file_name)
-                # Construct the destination path
-                destination_path = os.path.join(destination_directory, file_name)
+            destination_path = get_full_path(file_name, pattern, destination_directory)
+            file_path = os.path.join(root, file_name)
+            if not destination_path:
+                continue
 
-                # Handle possible name collision in the destination directory
-                counter = 1
-                while os.path.exists(destination_path):
-                    destination_path = os.path.join(destination_directory, f"{file_name[:-4]}_{counter}{file_name[-4:]}")
-                    counter += 1
+            # Handle possible name collision in the destination directory
+            counter = 1
+            while os.path.exists(destination_path):
+                destination_path = os.path.join(destination_directory, f"{file_name[:-4]}_{counter}{file_name[-4:]}")
+                counter += 1
 
-                # Move the file
-                shutil.move(file_path, destination_path)
-                print(f"Moved: {file_path} -> {destination_path}")
+            # Move the file
+            shutil.move(file_path, destination_path)
+            print(f"Moved: {file_path} -> {destination_path}")
 
 # Source directory to search
-source_directory = '/Users/miethe/Downloads/browser-logos-74.0.0/src/node.js'
-
-# Destination directory to move files
-destination_directory = '/Users/miethe/dev/openshift-practice/logos/SVG-Logos/Browsers'
+source_directory = '/Users/miethe/Downloads/abrahamcalf'
 
 # Regex pattern to match files that should not be moved
-pattern = r'[0-9]x[0-9]\.png'
+#pngImagePattern = r"[0-9]*x[0-9]*\.png"
 
 # Call the function to move files
-move_files(source_directory, destination_directory, pattern)
+move_files(source_directory, SVG_STRING, SVG_DESTINATION_DIRECTORY)
